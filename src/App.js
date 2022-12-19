170887e44f44580afffe6ba106ef9515f91558b7
wattoat <sandeepkharival123@gmail.com>
2022-12-19 20:29:06 +0530

    changes made in burgershop

--- src/App.js
+++ src/App.js
@@ -30,8 +30,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/about/About' element={<About/> }></Route>
+        <Route path='/me' element={<Login/>}></Route>
+        <Route path='/about' element={<About/> }></Route>
         <Route path='*' element={<Notfound/> }></Route> 
       </Routes>
       <Footer/>
